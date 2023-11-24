f998a2d426459cb3c7fc873a126d06f456d9d609
Kanade <98863311+Kanaede@users.noreply.github.com>
2023-11-24 21:52:22 +0700

    Initial commit

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,3 +1,9 @@
 //import mediumZoom from 'https://cdn.jsdelivr.net/npm/medium-zoom@1.1.0/dist/medium-zoom.min.js'
 
-mediumZoom('#zoom-default')
+const zoomDefault = mediumZoom('#zoom-default', {
+  background: "#212529",
+})
+const zoomM50 = mediumZoom('#zoom-default', {
+  background: "#212529",
+  margin: 50,
+})
