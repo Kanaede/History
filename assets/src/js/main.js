135d9c5c548c74d8a835014f6ae1e87e71f50aa7
Kanade <98863311+Kanaede@users.noreply.github.com>
2023-11-24 19:03:37 +0700

    Initial commit

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,5 +1,25 @@
-const mediumZoom = document.getElementById('zoom')
+import mediumZoom from 'medium-zoom'
 
-mediumZoom('.zoom', {
-  margin: 50
+const zoomDefault = mediumZoom('#zoom-default')
+
+const observedZooms = [
+  zoomDefault,
+]
+
+const history = document.querySelector('#history')
+
+observedZooms.forEach(zoom => {
+  zoom.on('open', event => {
+    const time = new Date().toLocaleTimeString()
+    history.innerHTML += `<li>Image "<em>${
+      event.target.alt
+    }</em>" was zoomed at ${time}</li>`
+  })
+
+  zoom.on('detach', event => {
+    const time = new Date().toLocaleTimeString()
+    history.innerHTML += `<li>Image <em>"${
+      event.target.alt
+    }"</em> was detached at ${time}</li>`
+  })
 })
